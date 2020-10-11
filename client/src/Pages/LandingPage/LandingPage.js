6a1627fadd2eb5afec62c116440feb0e6d862aac
ThomasFoydel <thomasjfoydel@gmail.com>
2020-10-10 23:58:38 -0500

    remove schedule, add landing title

--- client/src/Pages/LandingPage/LandingPage.js
+++ client/src/Pages/LandingPage/LandingPage.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const LandingPage = ({ setCurrentShow, setAuthOpen }) => {
   return (
     <div className='landingpage'>
-      <h2>LandingPage</h2>
+      <h2 className="landing-title">FITRA</h2>
       <SignInSignUp setAuthOpen={setAuthOpen} setCurrentShow={setCurrentShow} />
 
       <Link to='/coachportal' className='coachportal-btn'>
