0d5300d0bf542611d6ad9233079c4e4167b0cdcb
ThomasFoydel <thomasjfoydel@gmail.com>
2020-11-03 17:13:03 -0600

    mobile navbar

--- client/src/Components/NavBar/MobileNavBar.js
+++ client/src/Components/NavBar/MobileNavBar.js
@@ -1,46 +1,76 @@
 import React, { useContext, useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { CTX } from 'context/Store';
+import hamburger from 'imgs/icons/hamburger.svg';
+import { useSpring, animated, config } from 'react-spring';
 
 const MobileNavBar = ({
   props: { isLoggedIn, openLogin, trainerExt, logout, isTrainer },
 }) => {
   const [appState, updateState] = useContext(CTX);
+  const [sideBarOpen, setSideBarOpen] = useState(false);
+
+  const sideBarAnimation = useSpring({
+    left: sideBarOpen ? 0 : -280,
+    // top: sideBarOpen ? '5.9rem' : '-100%',
+    // opacity: sideBarOpen ? 1 : 0,
+    config: config.smooth,
+  });
 
   return (
-    <div className='mobile-navbar'>
-      <Link to={isLoggedIn ? `${trainerExt}/` : '/'} className='home-link'>
-        <h2 className='logo-title'>FITRA</h2>
-      </Link>
-
-      {!isTrainer && (
-        <Link to='/trainers' className='link'>
-          Trainers
+    <div>
+      <div className='white-border'></div>
+      <animated.div
+        className='mobile-sidebar'
+        style={sideBarAnimation}
+        onClick={() => setSideBarOpen(false)}
+        // style={{ display: sideBarOpen ? 'flex' : 'none' }}
+      >
+        {!isTrainer && (
+          <Link to='/trainers' className='link'>
+            Trainers
+          </Link>
+        )}
+        {!isLoggedIn && (
+          <>
+            <button onClick={openLogin} className='link login-btn'>
+              Login
+            </button>
+          </>
+        )}
+        {isLoggedIn && (
+          <>
+            <Link to={`${trainerExt}/schedule`} className='link'>
+              Schedule
+            </Link>
+            <Link to={`${trainerExt}/messages`} className='link'>
+              Messages
+            </Link>
+            <Link
+              to={`/${isTrainer ? 'trainer' : 'user'}/${appState.user.id}`}
+              className='link'
+            >
+              Profile
+            </Link>
+            <Link to={`${trainerExt}/settings`} className='link'>
+              Settings
+            </Link>
+            <button className='logout-btn' onClick={logout}>
+              <div className='link'>Logout</div>
+            </button>
+          </>
+        )}
+      </animated.div>
+      <div className='mobile-navbar1'>
+        <Link to={isLoggedIn ? `${trainerExt}/` : '/'} className='home-link'>
+          <h2 className='logo-title'>FITRA</h2>
         </Link>
-      )}
-      {!isLoggedIn && (
-        <>
-          <Link to={`${trainerExt}/schedule`} className='link'>
-            Schedule
-          </Link>
-          <Link to={`${trainerExt}/messages`} className='link'>
-            Messages
-          </Link>
-          <Link
-            to={`/${isTrainer ? 'trainer' : 'user'}/${appState.user.id}`}
-            className='link'
-          >
-            Profile
-          </Link>
-          <Link to={`${trainerExt}/settings`} className='link'>
-            Settings
-          </Link>
-
-          <button onClick={openLogin} className='link login-btn'>
-            Login
-          </button>
-        </>
-      )}
+        <img
+          src={hamburger}
+          onClick={() => setSideBarOpen((o) => !o)}
+          className='open-btn'
+        />
+      </div>
     </div>
   );
 };
