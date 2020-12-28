eee930db24a2c85dd764dd99d95d3c30e94f979f
ThomasFoydel <thomasjfoydel@gmail.com>
2020-12-27 21:49:54 -0600

    static file declaration, heroku path error

--- index.js
+++ index.js
@@ -39,9 +39,9 @@ app.use(cors());
 
 /* production mode */
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join((__dirname = 'client/build/index.html')));
+    res.sendFile(path.join((__dirname = '/client/build/index.html')));
   });
 }
 
