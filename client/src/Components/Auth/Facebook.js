2a785a02357a41c88af415bbba7052b0123cc6ed
ThomasFoydel <thomasjfoydel@gmail.com>
2021-01-02 15:51:53 -0600

    fb autoload

--- client/src/Components/Auth/Facebook.js
+++ client/src/Components/Auth/Facebook.js
@@ -6,12 +6,7 @@ const Facebook = ({ callback }) => {
     <FacebookLogin
       appId={`${process.env.FACEBOOK_ID}`}
       callback={callback}
-      render={(renderProps) => (
-        <button onClick={renderProps.onClick} className='fb-btn'>
-          <i className='fab fa-facebook' />
-          <span className='ml-4'>Sign In with Facebook</span>
-        </button>
-      )}
+      autoLoad={true}
     />
   );
 };
