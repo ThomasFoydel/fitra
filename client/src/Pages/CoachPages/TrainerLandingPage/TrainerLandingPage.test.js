104a116cd37e8822e5e0e32566a6bf84659e0242
ThomasFoydel <thomasjfoydel@gmail.com>
2021-02-07 17:40:19 -0600

    trainer landing page tests

---
+++ client/src/Pages/CoachPages/TrainerLandingPage/TrainerLandingPage.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, screen, waitFor, cleanup } from '@testing-library/react';
+
+import Store from 'context/Store';
+import TrainerLandingPage from './TrainerLandingPage';
+
+describe('Trainer landing page', () => {
+  beforeEach(async () => {
+    render(
+      <Store>
+        <TrainerLandingPage />
+      </Store>
+    );
+  });
+  afterEach(cleanup);
+
+  it('Should display a button that says "Get Started"', async () => {
+    await screen.getByRole('button');
+    await screen.getByText('Get Started');
+  });
+
+  it('Should have heading containing pitch text and button', () => {
+    const heading = screen.getByRole('heading');
+    expect(heading.innerHTML).toEqual(
+      'STREAMLINE<br>YOUR<br>WORKFLOW<button>Get Started</button>'
+    );
+  });
+
+  it('Should have an image', async () => {
+    await screen.getByRole('img');
+  });
+});
