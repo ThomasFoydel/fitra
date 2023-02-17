16f7ae82e70e29ef0cd42b8d105e98c2678ded35
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-17 17:25:57 -0500

    organized trainer home

--- client/src/Pages/CoachPages/TrainerHome/TrainerHome.js
+++ client/src/Pages/CoachPages/TrainerHome/TrainerHome.js
@@ -1,46 +1,45 @@
-import React, { useContext, useEffect, useState } from 'react';
-import axios from 'axios';
-import { config, animated, useTransition } from 'react-spring';
-import Session from './Session';
-import { CTX } from 'context/Store';
-import './TrainerHome.scss';
-import { act } from '@testing-library/react';
+import axios from 'axios'
+import { act } from '@testing-library/react'
+import { config, animated, useTransition } from 'react-spring'
+import React, { useContext, useEffect, useState } from 'react'
+import { CTX } from 'context/Store'
+import Session from './Session'
+import './TrainerHome.scss'
 
 const TrainerHome = () => {
-  const [appState] = useContext(CTX);
-  const { type } = appState.user;
-  const [foundSessions, setFoundSessions] = useState([]);
-  let { token } = appState.user;
+  const [{ user }] = useContext(CTX)
+  const { type, token } = user
+  const [foundSessions, setFoundSessions] = useState([])
 
   useEffect(() => {
-    let subscribed = true;
+    let subscribed = true
     axios
       .get(`/api/${type}/dashboard`, { headers: { 'x-auth-token': token } })
       .then(({ data: { sessions } }) => {
         if (subscribed)
           process.env.NODE_ENV === 'production'
             ? setFoundSessions(sessions)
-            : act(() => setFoundSessions(sessions));
+            : act(() => setFoundSessions(sessions))
       })
-      .catch((err) => console.log('connection error: ', err));
-    return () => (subscribed = false);
-  }, [token, type]);
+      .catch((err) => console.error('connection error: ', err))
+    return () => (subscribed = false)
+  }, [token, type])
 
   const animation = useTransition(foundSessions, (item) => item._id, {
+    trail: 200,
+    config: config.wobbly,
     from: { opacity: '0', transform: 'translateY(-20px)' },
     enter: { opacity: '1', transform: 'translateY(0px)' },
     leave: { opacity: '0', transform: 'translateY(-20px)' },
-    trail: 200,
-    config: config.wobbly,
-  });
+  })
 
   return (
     <>
-      <div className='background' />
-      <div className='overlay' />
-      <div className='trainer-home'>
+      <div className="background" />
+      <div className="overlay" />
+      <div className="trainer-home">
         <h2>Sessions</h2>
-        <div className='sessions'>
+        <div className="sessions">
           {foundSessions.length > 0 ? (
             animation.map(({ item, props, key }) => (
               <animated.div style={props} key={key}>
@@ -53,7 +52,7 @@ const TrainerHome = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default TrainerHome;
+export default TrainerHome
