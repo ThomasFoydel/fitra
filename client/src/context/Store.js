21c0c0ac1251cd61ba112ff23d34a3f79dffc0ea
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-17 18:19:47 -0500

    organied context store

--- client/src/context/Store.js
+++ client/src/context/Store.js
@@ -1,96 +1,83 @@
-import React from 'react';
+import { createContext, useReducer } from 'react'
 
-const CTX = React.createContext();
-export { CTX };
+export const CTX = createContext()
 
 export function reducer(state, action) {
-  let { payload } = action;
-  let {
-    user,
-    token,
-    profilePic,
-    coverPic,
-    kind,
-    message,
-    id,
-    value,
-    page,
-    type,
-    tags,
-    res,
-  } = payload || {};
+  const { payload } = action
+  const { id, res, tags, user, kind, page, type, value, token, message, coverPic, profilePic } =
+    payload || {}
   switch (action.type) {
     case 'LOGIN':
-      localStorage.setItem('fitr-token', token);
+      localStorage.setItem('fitr-token', token)
       return {
         ...state,
         isLoggedIn: true,
+        messages: user.messages,
+        settings: user.settings,
         user: {
+          token,
           id: user.id,
           bio: user.bio,
-          type: user.userType,
+          tags: user.tags,
           name: user.name,
-          displayEmail: user.displayEmail,
+          type: user.userType,
           coverPic: user.coverPic,
           profilePic: user.profilePic,
-          tags: user.tags,
-          token,
+          displayEmail: user.displayEmail,
         },
-        settings: user.settings,
-        messages: user.messages,
-      };
+      }
     case 'LOGOUT':
-      localStorage.removeItem('fitr-token');
-      if (window.FB) window.FB.logout();
+      localStorage.removeItem('fitr-token')
+      if (window.FB) window.FB.logout()
       return {
         ...state,
-        isLoggedIn: false,
         showAuth: false,
+        isLoggedIn: false,
         user: { name: '', displayEmail: '', userType: 'client' },
-      };
+      }
     case 'CHANGE_PIC':
-      let newPic = kind === 'coverPic' ? coverPic : profilePic;
+      const newPic = kind === 'coverPic' ? coverPic : profilePic
       return {
         ...state,
         user: { ...state.user, [kind]: newPic },
-      };
+      }
     case 'NEW_MESSAGE':
-      let copy = { ...state.messages };
-      let other = message.participants.filter((p) => p !== state.user.id)[0];
-      let thread = copy[other];
-      let updatedThread = thread ? [...thread, message] : [message];
-      let updatedMessages = { ...copy, [other]: updatedThread };
-      return { ...state, messages: updatedMessages };
+      const copy = { ...state.messages }
+      const other = message.participants.filter((p) => p !== state.user.id)[0]
+      const thread = copy[other]
+      const updatedThread = thread ? [...thread, message] : [message]
+      const updatedMessages = { ...copy, [other]: updatedThread }
+      return { ...state, messages: updatedMessages }
     case 'CHANGE_SETTINGS':
-      return { ...state, settings: { ...state.settings, [type]: value } };
+      return { ...state, settings: { ...state.settings, [type]: value } }
     case 'CHANGE_SETTING':
-      return { ...state, settings: { ...state.settings, [id]: value } };
+      return { ...state, settings: { ...state.settings, [id]: value } }
     case 'TOGGLE_AUTH':
-      return { ...state, showAuth: !state.showAuth };
+      return { ...state, showAuth: !state.showAuth }
     case 'CHANGE_AUTH_PAGE':
-      return { ...state, authPage: page };
+      return { ...state, authPage: page }
     case 'CHANGE_AUTH_TYPE':
-      return { ...state, authType: type };
+      return { ...state, authType: type }
     case 'CHANGE_TAGS':
-      return { ...state, user: { ...state.user, tags } };
+      return { ...state, user: { ...state.user, tags } }
     case 'EDIT_PROFILE':
-      return { ...state, user: { ...state.user, ...res } };
+      return { ...state, user: { ...state.user, ...res } }
     default:
-      console.log('REDUCER ERROR: action: ', action);
-      return { ...state };
+      console.log('REDUCER ERROR: action: ', action)
+      return { ...state }
   }
 }
 
 export default function Store(props) {
-  const stateHook = React.useReducer(reducer, {
-    isLoggedIn: false,
-    showAuth: false,
-    authPage: 'register',
-    authType: 'client',
-    user: { name: '', displayEmail: '', type: 'client', token: null, id: null },
+  const stateHook = useReducer(reducer, {
     messages: {},
+    showAuth: false,
+    isLoggedIn: false,
+    authType: 'client',
+    authPage: 'register',
     settings: { darkmode: false, rate: '30', currency: 'USD' },
-  });
+    user: { name: '', displayEmail: '', type: 'client', token: null, id: null },
+  })
 
-  return <CTX.Provider value={stateHook}>{props.children}</CTX.Provider>;
+  return <CTX.Provider value={stateHook}>{props.children}</CTX.Provider>
 }
