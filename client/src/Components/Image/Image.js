572a6a66c437c12643e15d020b42b07e00201113
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-17 18:43:50 -0500

    organized image component

--- client/src/Components/Image/Image.js
+++ client/src/Components/Image/Image.js
@@ -1,46 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import loadingGif from 'imgs/loading/spin.gif';
-import defaultProfile from 'imgs/default/profile.jpg';
+import PropTypes from 'prop-types'
+import React, { useState, useEffect } from 'react'
+import defaultProfile from 'imgs/default/profile.jpg'
+import loadingGif from 'imgs/loading/spin.gif'
 
 const Image = ({ name, src, style, alt }) => {
-  const [loading, setLoading] = useState(true);
-  const [source, setSource] = useState(src || defaultProfile);
-  const [err, setErr] = useState(false);
+  const [loading, setLoading] = useState(true)
+  const [source, setSource] = useState(src || defaultProfile)
+  const [err, setErr] = useState(false)
 
   useEffect(() => {
-    if (err) setSource(defaultProfile);
-  }, [err]);
+    if (err) setSource(defaultProfile)
+  }, [err])
 
   useEffect(() => {
-    if (src) setSource(src);
-    else setSource(defaultProfile);
-  }, [src]);
+    if (src) setSource(src)
+    else setSource(defaultProfile)
+  }, [src])
 
   return (
     <img
+      alt={alt}
       style={style}
       className={name}
-      alt={alt}
+      onError={() => setErr(true)}
       src={loading ? loadingGif : source}
-      onError={(err) => {
-        console.log({ err });
-        setErr(true);
-      }}
-      onLoad={() => {
-        setTimeout(() => {
-          setLoading(false);
-        }, 200);
-      }}
+      onLoad={() => setTimeout(() => setLoading(false), 200)}
     />
-  );
-};
+  )
+}
 
 Image.propTypes = {
+  style: PropTypes.object,
   src: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  style: PropTypes.object,
-};
+}
 
-export default Image;
+export default Image
