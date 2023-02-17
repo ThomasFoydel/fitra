99182e44278ea83cd6fd2565abe6cfe314e29850
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-17 14:04:39 -0500

    organized util

--- client/src/util/util.js
+++ client/src/util/util.js
@@ -1,12 +1,4 @@
-export const days = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-];
+export const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 export const halfHours = [
   '12:00 AM',
@@ -33,7 +25,6 @@ export const halfHours = [
   '10:30 AM',
   '11:00 AM',
   '11:30 AM',
-
   '12:00 PM',
   '12:30 PM',
   '1:00 PM',
@@ -59,99 +50,94 @@ export const halfHours = [
   '11:00 PM',
   '11:30 PM',
   '12:00 AM',
-];
+]
 
 export const getOneHalfHourAhead = (hour) => {
-  let index = halfHours.indexOf(hour);
-  return halfHours[index + 1];
-};
+  const index = halfHours.indexOf(hour)
+  return halfHours[index + 1]
+}
 
 export const setUpWeek = (weekShift) => {
-  const mSecondsInADay = 86400000;
-  const mSecondsInAWeek = 604800000;
+  const mSecondsInADay = 86400000
+  const mSecondsInAWeek = 604800000
 
-  let current = new Date();
-  const dayOfWeek = current.getDay();
-  let currentTime = current.getTime();
+  const current = new Date()
+  const dayOfWeek = current.getDay()
+  const currentTime = current.getTime()
 
   /* nowDate = the current day of the week, shifted to whatever the current week is */
-  let nowDate = new Date(currentTime + mSecondsInAWeek * weekShift);
-  let now = nowDate.getTime();
+  const nowDate = new Date(currentTime + mSecondsInAWeek * weekShift)
+  const now = nowDate.getTime()
 
-  let weekObj = {};
+  const weekObj = {}
 
   /* fill in forwards to saturday */
   for (let i = dayOfWeek; i < 7; i++) {
-    const daysFromCurrent = i - dayOfWeek;
-    const mSecondsPassed = daysFromCurrent * mSecondsInADay;
-    const date = new Date(now + mSecondsPassed);
-    weekObj[i] = date;
+    const daysFromCurrent = i - dayOfWeek
+    const mSecondsPassed = daysFromCurrent * mSecondsInADay
+    const date = new Date(now + mSecondsPassed)
+    weekObj[i] = date
   }
+
   /* fill in backwards to monday, if day is not sunday */
   if (dayOfWeek > 0) {
-    let count = 1;
+    let count = 1
     for (let j = dayOfWeek; j > 0; j--) {
-      let daysBefore = count * mSecondsInADay;
-      let date = new Date(nowDate - daysBefore);
-      weekObj[j - 1] = date;
-      count++;
+      let daysBefore = count * mSecondsInADay
+      let date = new Date(nowDate - daysBefore)
+      weekObj[j - 1] = date
+      count++
     }
   }
-  return weekObj;
-};
+
+  return weekObj
+}
 
 export const checkBlock = (data, week) => {
-  if (data.recurring === true) return true;
-  let start = week[0];
-  let end = week[6];
+  if (data.recurring === true) return true
+  const start = week[0]
+  const end = week[6]
 
-  let startYear = start.getFullYear();
-  let startMonth = Number(start.getMonth());
-  if (startMonth < 10) startMonth = `0${startMonth}`;
-  let startDay = start.getDate();
-  let startWeek = new Date(startYear, startMonth, startDay, '00', '00', '00');
+  const startYear = start.getFullYear()
+  let startMonth = Number(start.getMonth())
+  if (startMonth < 10) startMonth = `0${startMonth}`
+  const startDay = start.getDate()
+  const startWeek = new Date(startYear, startMonth, startDay, '00', '00', '00')
 
-  let endYear = end.getFullYear();
-  let endMonth = end.getMonth();
-  if (endMonth < 10) endMonth = `0${endMonth}`;
-  let endDay = end.getDate();
-  let endWeek = new Date(endYear, endMonth, endDay, '23', '59', '59', '999');
+  const endYear = end.getFullYear()
+  let endMonth = end.getMonth()
+  if (endMonth < 10) endMonth = `0${endMonth}`
+  const endDay = end.getDate()
+  const endWeek = new Date(endYear, endMonth, endDay, '23', '59', '59', '999')
 
-  let block = new Date(data.startDate);
-  if (block >= startWeek && block < endWeek) {
-    return true;
-  } else return false;
-};
+  const block = new Date(data.startDate)
+  return block >= startWeek && block < endWeek
+}
 
 export const dateFromDateAndTime = (date, time, startTime) => {
-  let newYear = date.getFullYear();
-  let newMonth = Number(date.getMonth());
-  /* if (newMonth < 10) newMonth = `0${newMonth}`; */
-  let newDay = Number(date.getDate());
+  const newYear = date.getFullYear()
+  const newMonth = Number(date.getMonth())
+  let newDay = Number(date.getDate())
 
-  let split = time.split(':');
-  let newHour = Number(split[0]);
-  let afterNoon = split[1].split(' ')[1] === 'PM';
-  if (afterNoon && newHour !== 12) newHour += 12;
+  const splitTime = time.split(':')
+  let newHour = Number(splitTime[0])
+  const afterNoon = splitTime[1].split(' ')[1] === 'PM'
+  if (afterNoon && newHour !== 12) newHour += 12
   /* if it is 12:00am */
-  if (!afterNoon && newHour === 12) newHour = 0;
-  let newMin = split[1].split(' ')[0];
+  if (!afterNoon && newHour === 12) newHour = 0
+  const newMin = splitTime[1].split(' ')[0]
   /* if it is 12:00am and this is not a start time, increase day by 1 (its 12:00AM the next day) */
   if (startTime && newHour === 0) {
-    let startIndex = halfHours.indexOf(startTime);
-    if (startIndex > 0) {
-      newDay++;
-    }
+    const startIndex = halfHours.indexOf(startTime)
+    if (startIndex > 0) newDay++
   }
-  let newDate = new Date(newYear, newMonth, newDay, newHour, newMin);
-  return newDate;
-};
+  return new Date(newYear, newMonth, newDay, newHour, newMin)
+}
 
 export const getHalfHourFromDate = (date) => {
-  if (typeof date === 'string') date = new Date(date);
-  let sHour = date.getHours();
-  let sMinutes = date.getMinutes();
-  sHour *= 2;
-  if (sMinutes === 30) sHour += 1;
-  return halfHours[sHour];
-};
+  if (typeof date === 'string') date = new Date(date)
+  let sHour = date.getHours() * 2
+  const sMinutes = date.getMinutes()
+  if (sMinutes === 30) sHour += 1
+  return halfHours[sHour]
+}
