e84269a1bb7d8043ddb10802a391689c841269d8
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-17 18:07:11 -0500

    organized mobile coach schedule

--- client/src/Pages/CoachPages/Schedule/Mobile/MobileSchedule.js
+++ client/src/Pages/CoachPages/Schedule/Mobile/MobileSchedule.js
@@ -1,50 +1,48 @@
-import React from 'react';
-import { dateFromDateAndTime } from '../../../../util/util';
-import { v4 as uuidv4 } from 'uuid';
-import Day from './parts/Day';
+import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { dateFromDateAndTime } from '../../../../util/util'
+import Day from './parts/Day'
 
 const MobileSchedule = ({
   props: { days, week, displayBlocks, setDisplayBlocks, setActualBlocks },
 }) => {
   const handleSubmitTime = ({ timeSelection: { start, end }, day }) => {
-    const selectedDate = week[days.indexOf(day)];
-    const startTime = `${start.hour}:${start.minute} ${start.amOrPm}`;
-    const endTime = `${end.hour}:${end.minute} ${end.amOrPm}`;
-    const startDate = dateFromDateAndTime(selectedDate, startTime);
-    const endDate = dateFromDateAndTime(selectedDate, endTime);
+    const selectedDate = week[days.indexOf(day)]
+    const startTime = `${start.hour}:${start.minute} ${start.amOrPm}`
+    const startDate = dateFromDateAndTime(selectedDate, startTime)
+    const endTime = `${end.hour}:${end.minute} ${end.amOrPm}`
+    const endDate = dateFromDateAndTime(selectedDate, endTime)
 
-    if (startDate.getTime() > endDate.getTime())
-      return console.log('error: start time cannot be after end time');
+    if (startDate.getTime() > endDate.getTime()) {
+      return console.error('error: start time cannot be after end time')
+    }
 
     const newBlock = {
-      startDate,
-      endDate,
-      start: startTime,
-      end: endTime,
       day,
+      endDate,
       title: '',
-      recurring: false,
+      startDate,
       id: uuidv4(),
-    };
+      end: endTime,
+      recurring: false,
+      start: startTime,
+    }
 
-    setDisplayBlocks((displayBlocks) => [...displayBlocks, newBlock]);
-    setActualBlocks((actualBlocks) => [...actualBlocks, newBlock]);
-  };
+    setDisplayBlocks((displayBlocks) => [...displayBlocks, newBlock])
+    setActualBlocks((actualBlocks) => [...actualBlocks, newBlock])
+  }
 
   return (
-    <div className='mb-schedule'>
-      <h3 className='title'>schedule</h3>
-      {days.map((day, i) => {
-        const currentDate = new Date(week[i]);
-        return (
-          <Day
-            props={{ currentDate, day, displayBlocks, handleSubmitTime }}
-            key={day}
-          />
-        );
-      })}
+    <div className="mb-schedule">
+      <h3 className="title">schedule</h3>
+      {days.map((day, i) => (
+        <Day
+          key={day}
+          props={{ currentDate: new Date(week[i]), day, displayBlocks, handleSubmitTime }}
+        />
+      ))}
     </div>
-  );
-};
+  )
+}
 
-export default MobileSchedule;
+export default MobileSchedule
--- client/src/Pages/CoachPages/Schedule/Mobile/parts/TimeInput.js
+++ client/src/Pages/CoachPages/Schedule/Mobile/parts/TimeInput.js
@@ -1,20 +1,18 @@
-import React from 'react';
+import React from 'react'
 
-const hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 const TimeInput = ({ props: { handleTimeSelect, label, value } }) => {
-  const { hour, minute, amOrPm } = value;
+  const { hour, minute, amOrPm } = value
 
-  const handleChange = ({ target: { value, id } }) => {
-    console.log({ value });
-    handleTimeSelect({ value, id, label });
-  };
+  const handleChange = ({ target: { value, id } }) => handleTimeSelect({ value, id, label })
+
   return (
-    <div className='timeinput'>
-      <div className='label'>{label}</div>
-      <div className='selectors'>
+    <div className="timeinput">
+      <div className="label">{label}</div>
+      <div className="selectors">
         <h6>hr</h6>
-        <select onChange={handleChange} id='hour' value={hour}>
+        <select onChange={handleChange} id="hour" value={hour}>
           {hours.map((h) => (
             <option key={h} value={h}>
               {h}
@@ -22,18 +20,18 @@ const TimeInput = ({ props: { handleTimeSelect, label, value } }) => {
           ))}
         </select>
         <h6>min</h6>
-        <select onChange={handleChange} id='minute' value={minute}>
-          <option value='00'>00</option>
-          <option value='30'>30</option>
+        <select onChange={handleChange} id="minute" value={minute}>
+          <option value="00">00</option>
+          <option value="30">30</option>
         </select>
 
-        <select onChange={handleChange} id='amOrPm' value={amOrPm}>
-          <option value='AM'>AM</option>
-          <option value='PM'>PM</option>
+        <select onChange={handleChange} id="amOrPm" value={amOrPm}>
+          <option value="AM">AM</option>
+          <option value="PM">PM</option>
         </select>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default TimeInput;
+export default TimeInput
