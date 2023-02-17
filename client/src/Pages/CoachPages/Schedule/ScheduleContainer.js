994304a3c56341fc31c39f84964327fb9f48b80f
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-17 17:43:04 -0500

    organized coach settings

--- client/src/Pages/CoachPages/Schedule/ScheduleContainer.js
+++ client/src/Pages/CoachPages/Schedule/ScheduleContainer.js
@@ -1,128 +1,115 @@
-import React, { useState, useEffect, useContext, useRef } from 'react';
-import Schedule from './Schedule';
-import axios from 'axios';
-import { CTX } from 'context/Store';
-import { getHalfHourFromDate, days } from '../../../util/util';
-import { act } from '@testing-library/react';
+import axios from 'axios'
+import { act } from '@testing-library/react'
+import React, { useState, useEffect, useContext, useRef } from 'react'
+import { getHalfHourFromDate, days } from '../../../util/util'
+import { CTX } from 'context/Store'
+import Schedule from './Schedule'
 
 const ScheduleContainer = () => {
-  const [appState] = useContext(CTX);
-  let { token } = appState.user;
-  const [entries, setEntries] = useState(null);
-  const [sessions, setSessions] = useState(null);
-  const [min, setMin] = useState(1);
-  const [max, setMax] = useState(3);
-  const [err, setErr] = useState('');
+  const [{ user }] = useContext(CTX)
+  const { token } = user
+  const [entries, setEntries] = useState(null)
+  const [sessions, setSessions] = useState(null)
+  const [min, setMin] = useState(1)
+  const [max, setMax] = useState(3)
+  const [err, setErr] = useState('')
 
   const handleChange = (e) => {
     axios
-      .put('/api/trainer/schedule/', e, {
-        headers: { 'x-auth-token': token },
-      })
+      .put('/api/trainer/schedule/', e, { headers: { 'x-auth-token': token } })
       .then(({ data }) => setEntries(data))
-      .catch((err) => console.log({ err }));
-  };
+      .catch((err) => console.error({ err }))
+  }
 
   useEffect(() => {
-    let subscribed = true;
+    let subscribed = true
     axios
       .get('/api/trainer/schedule/', { headers: { 'x-auth-token': token } })
       .then(({ data: { entries, min, max, foundSessions } }) => {
         if (foundSessions) {
-          let sessions = [];
+          const sessions = []
           foundSessions.forEach(({ client, startTime, endTime, _id }) => {
-            if (typeof startTime === 'string') startTime = new Date(startTime);
-            let day = days[startTime.getDay()];
-            let startHour = getHalfHourFromDate(startTime);
-            let endHour = getHalfHourFromDate(endTime);
-            let newSession = {
-              startDate: startTime,
-              endDate: endTime,
-              start: startHour,
-              end: endHour,
+            if (typeof startTime === 'string') startTime = new Date(startTime)
+            const day = days[startTime.getDay()]
+            const startHour = getHalfHourFromDate(startTime)
+            const endHour = getHalfHourFromDate(endTime)
+            const newSession = {
               day,
-              title: '',
-              recurring: false,
-              id: _id,
-              session: true,
               client,
-            };
-            sessions.push(newSession);
-          });
-          if (process.env.NODE_ENV === 'production') {
-            setSessions(sessions);
-          } else {
-            act(() => setSessions(sessions));
-          }
+              id: _id,
+              title: '',
+              end: endHour,
+              session: true,
+              start: startHour,
+              endDate: endTime,
+              recurring: false,
+              startDate: startTime,
+            }
+            sessions.push(newSession)
+          })
+          if (!subscribed) return
+          if (process.env.NODE_ENV === 'production') setSessions(sessions)
+          else act(() => setSessions(sessions))
         }
-        if (subscribed) {
-          if (process.env.NODE_ENV === 'production') {
-            setEntries(entries);
-            setMin(min);
-            setMax(max);
-          } else {
-            act(() => {
-              setEntries(entries);
-              setMin(min);
-              setMax(max);
-            });
-          }
+        if (!subscribed) return
+        if (process.env.NODE_ENV === 'production') {
+          setEntries(entries)
+          setMin(min)
+          setMax(max)
+        } else {
+          act(() => {
+            setEntries(entries)
+            setMin(min)
+            setMax(max)
+          })
         }
-      });
-    return () => (subscribed = false);
-  }, [token]);
+      })
+    return () => (subscribed = false)
+  }, [token])
 
   const handleMinMax = ({ target: { value, id } }) => {
-    value = Number(value);
-    if (id === 'maximum' && value < min)
-      return setErr('maximum must be greater than minimum');
-    if (id === 'minimum' && value > max)
-      return setErr('minimum cannot be greater than maximum');
+    value = Number(value)
+    if (id === 'maximum' && value < min) return setErr('maximum must be greater than minimum')
+    if (id === 'minimum' && value > max) return setErr('minimum cannot be greater than maximum')
     axios
-      .put(
-        `/api/trainer/minmax/${id}/`,
-        { value },
-        {
-          headers: { 'x-auth-token': token },
-        }
-      )
+      .put(`/api/trainer/minmax/${id}/`, { value }, { headers: { 'x-auth-token': token } })
       .then(({ data: { min, max, err } }) => {
-        if (err) return setErr(err);
-        setMin(min);
-        setMax(max);
+        if (err) return setErr(err)
+        setMin(min)
+        setMax(max)
       })
-      .catch((err) => console.log({ err }));
-  };
+      .catch((err) => console.error({ err }))
+  }
 
-  const didMountRef = useRef(false);
+  const didMountRef = useRef(false)
   useEffect(() => {
-    let subscribed = true;
+    let subscribed = true
     if (didMountRef.current) {
       setTimeout(() => {
-        if (subscribed) setErr('');
-      }, 2700);
-    } else didMountRef.current = true;
-    return () => (subscribed = false);
-  }, [err]);
+        if (subscribed) setErr('')
+      }, 2700)
+    } else didMountRef.current = true
+    return () => (subscribed = false)
+  }, [err])
 
   return (
-    <div className='schedule-container'>
+    <div className="schedule-container">
       {entries && (
         <Schedule
           props={{
             min,
-            handleMinMax,
             max,
-            change: handleChange,
-            entries,
             err,
+            entries,
             sessions,
             setSessions,
+            handleMinMax,
+            change: handleChange,
           }}
         />
       )}
     </div>
-  );
-};
+  )
+}
 
-export default ScheduleContainer;
+export default ScheduleContainer
--- client/src/Pages/CoachPages/Settings/Settings.js
+++ client/src/Pages/CoachPages/Settings/Settings.js
@@ -1,59 +1,57 @@
-import React, { useState, useContext } from 'react';
-import './Settings.scss';
-import { CTX } from 'context/Store';
-import { Link } from 'react-router-dom';
-import TagEditor from './settingForms/TagEditor';
-import RateEditor from './settingForms/RateEditor';
-import Active from './settingForms/Active';
+import { Link } from 'react-router-dom'
+import React, { useState, useContext } from 'react'
+import TagEditor from './settingForms/TagEditor'
+import RateEditor from './settingForms/RateEditor'
+import Active from './settingForms/Active'
+import { CTX } from 'context/Store'
+import './Settings.scss'
 
 const Settings = () => {
-  const [appState, updateState] = useContext(CTX) || [];
-  const [err, setErr] = useState('');
-  const { type, token, id } = appState.user || {};
-  const { rate, active } = appState.settings || {};
+  const [{ user, settings }, updateState] = useContext(CTX) || []
+  const { rate, active } = settings || {}
+  const { type, token, id } = user || {}
+  const [err, setErr] = useState('')
 
   const onComplete = ({ type, value }) => {
-    updateState({ type: 'CHANGE_SETTINGS', payload: { type, value } });
-  };
+    updateState({ type: 'CHANGE_SETTINGS', payload: { type, value } })
+  }
 
-  const onError = ({ err }) => {
-    setErr(err);
-  };
+  const onError = ({ err }) => setErr(err)
 
   return (
     <>
-      <div className='background' />
-      <div className='overlay' />
+      <div className="background" />
+      <div className="overlay" />
       <div className={`trainer-settings `}>
-        <h2 className='header center'>Settings</h2>
+        <h2 className="header center">Settings</h2>
 
-        <div className='form'>
-          <div className='setting-item'>
+        <div className="form">
+          <div className="setting-item">
             <Active props={{ onError, type, onComplete, token, active }} />
           </div>
-          <div className='setting-item'>
+          <div className="setting-item">
             <TagEditor />
           </div>
 
-          <div className='setting-item'>
+          <div className="setting-item">
             <RateEditor props={{ id, rate, onComplete, onError, token }} />
           </div>
 
-          <div className='setting-item'>
-            <Link to='/terms-of-use'>terms of use</Link>
+          <div className="setting-item">
+            <Link to="/terms-of-use">terms of use</Link>
           </div>
-          <div className='setting-item'>
-            <Link to='/privacy-policy'>privacy policy</Link>
+          <div className="setting-item">
+            <Link to="/privacy-policy">privacy policy</Link>
           </div>
-          <div className='setting-item'>
-            <Link className='delete-btn' to='/delete_my_account'>
+          <div className="setting-item">
+            <Link className="delete-btn" to="/delete_my_account">
               delete my account
             </Link>
           </div>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Settings;
+export default Settings
