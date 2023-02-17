62c3a7fe48aa20dab66cfd26ca2199009a0a6f28
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-17 18:13:31 -0500

    organized manage session page

--- client/src/Pages/CoachPages/ManageSession/ManageSession.js
+++ client/src/Pages/CoachPages/ManageSession/ManageSession.js
@@ -1,125 +1,113 @@
-import React, { useEffect, useState, useContext, useRef } from 'react';
-import axios from 'axios';
-import { CTX } from 'context/Store';
-import './ManageSession.scss';
-import { Navigate, Link } from 'react-router-dom';
-import Image from 'Components/Image/Image';
-import { act } from '@testing-library/react';
+import axios from 'axios'
+import { act } from '@testing-library/react'
+import { Navigate, Link, useParams } from 'react-router-dom'
+import React, { useEffect, useState, useContext, useRef } from 'react'
+import Image from 'Components/Image/Image'
+import { CTX } from 'context/Store'
+import './ManageSession.scss'
 
-const ManageSession = ({
-  match: {
-    params: { id },
-  },
-}) => {
-  const [appState] = useContext(CTX);
-  const [found, setFound] = useState(null);
-  const [err, setErr] = useState('');
-  const [openCancel, setOpenCancel] = useState(false);
-  let { token } = appState.user;
-  let [deleted, setDeleted] = useState(false);
+const ManageSession = () => {
+  const { id } = useParams()
+  const [{ user }] = useContext(CTX)
+  const { token } = user
+  const [openCancel, setOpenCancel] = useState(false)
+  const [deleted, setDeleted] = useState(false)
+  const [found, setFound] = useState(null)
+  const [err, setErr] = useState('')
+  const didMountRef = useRef(false)
 
-  const didMountRef = useRef(false);
   useEffect(() => {
-    let subscribed = true;
+    let subscribed = true
     if (didMountRef.current) {
       setTimeout(() => {
-        if (subscribed) setErr('');
-      }, 2700);
-    } else didMountRef.current = true;
-    return () => (subscribed = false);
-  }, [err]);
+        if (subscribed) setErr('')
+      }, 2700)
+    } else didMountRef.current = true
+    return () => (subscribed = false)
+  }, [err])
 
   useEffect(() => {
-    let subscribed = true;
+    let subscribed = true
     axios
-      .get(`/api/trainer/session/${id}`, {
-        headers: { 'x-auth-token': token },
-      })
+      .get(`/api/trainer/session/${id}`, { headers: { 'x-auth-token': token } })
       .then(({ data: { foundSession, err, foundClient } }) => {
-        if (subscribed)
-          if (err) setErr(err);
-          else if (process.env.NODE_ENV === 'production') {
-            setFound({ session: foundSession, client: foundClient });
-          } else {
-            act(() => setFound({ session: foundSession, client: foundClient }));
-          }
-      });
-    return () => (subscribed = false);
-  }, [token, id]);
+        if (!subscribed) return
+        if (err) setErr(err)
+        else if (process.env.NODE_ENV === 'production') {
+          setFound({ session: foundSession, client: foundClient })
+        } else {
+          act(() => setFound({ session: foundSession, client: foundClient }))
+        }
+      })
+    return () => (subscribed = false)
+  }, [token, id])
 
   const handleCancel = () => {
     axios
-      .delete(`/api/trainer/cancel-session/`, {
-        headers: { 'x-auth-token': token, id },
-      })
+      .delete(`/api/trainer/cancel-session/`, { headers: { 'x-auth-token': token, id } })
       .then(({ data: { id, err } }) => {
-        if (err) setErr(err);
-        else if (id) setDeleted(true);
+        if (err) setErr(err)
+        else if (id) setDeleted(true)
       })
-      .catch((err) => console.log({ err }));
-  };
+      .catch((err) => console.error({ err }))
+  }
 
-  let { startTime, endTime, _id, client } = found ? found.session : {};
-  let { name } = found ? found.client : {};
-  let start = new Date(startTime);
-  let end = new Date(endTime);
-  let currentTime = new Date(Date.now());
-  let started = currentTime > start;
-  let ended = currentTime > end;
-  let active = started && !ended;
+  const { startTime, endTime, _id, client } = found ? found.session : {}
+  const { name } = found?.client || {}
+  const start = new Date(startTime)
+  const end = new Date(endTime)
+  const currentTime = new Date(Date.now())
+  const started = currentTime > start
+  const ended = currentTime > end
+  const active = started && !ended
 
   return (
-    <div className='manage-session'>
-      {deleted && <Navigate to='/coachportal/schedule' />}
+    <div className="manage-session">
+      {deleted && <Navigate to="/coachportal/schedule" />}
       {found && (
         <>
-          <Link to='/coachportal/schedule'>
-            <i className='fas fa-angle-left fa-4x back-link'></i>
+          <Link to="/coachportal/schedule">
+            <i className="fas fa-angle-left fa-4x back-link"></i>
           </Link>
           <Link to={`/user/${client}`}>
             <Image
-              name='profile-pic'
+              name="profile-pic"
               alt="client's profile"
               src={`/api/image/user/profilePic/${client}`}
             />
           </Link>
           <Link to={`/user/${client}`}>
-            <div className='name'>{name}</div>
+            <div className="name">{name}</div>
           </Link>
-          <div className='date'>{start && start.toLocaleDateString()}</div>
-          <div className='start'>
-            start: {start && start.toLocaleTimeString()}
-          </div>
-          <div className='end'>end: {end && end.toLocaleTimeString()}</div>
+          <div className="date">{start && start.toLocaleDateString()}</div>
+          <div className="start">start: {start && start.toLocaleTimeString()}</div>
+          <div className="end">end: {end && end.toLocaleTimeString()}</div>
           {active && (
-            <Link className='connect-link' to={`/connect/${_id}`}>
+            <Link className="connect-link" to={`/connect/${_id}`}>
               connect
             </Link>
           )}
-          <div className='err'>{err}</div>
+          <div className="err">{err}</div>
 
           {openCancel ? (
-            <div className='cancel'>
+            <div className="cancel">
               <p>cancel this session?</p>
-              <button
-                className='close-btn'
-                onClick={() => setOpenCancel(false)}
-              >
+              <button className="close-btn" onClick={() => setOpenCancel(false)}>
                 nevermind
               </button>
-              <button className='confirm-btn' onClick={handleCancel}>
+              <button className="confirm-btn" onClick={handleCancel}>
                 confirm
               </button>
             </div>
           ) : (
-            <button className='cancel-btn' onClick={() => setOpenCancel(true)}>
+            <button className="cancel-btn" onClick={() => setOpenCancel(true)}>
               cancel session
             </button>
           )}
         </>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default ManageSession;
+export default ManageSession
--- client/src/Pages/CoachPages/ManageSession/ManageSession.test.js
+++ client/src/Pages/CoachPages/ManageSession/ManageSession.test.js
@@ -1,41 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { render, screen, waitFor, cleanup } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from 'react'
+import userEvent from '@testing-library/user-event'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { render, screen, waitFor, cleanup } from '@testing-library/react'
+import ManageSession from './ManageSession'
+import axios from '__mocks__/axios'
+import Store from 'context/Store'
 
-import axios from '__mocks__/axios';
-import Store from 'context/Store';
-import ManageSession from './ManageSession';
-
-const exampleUser = { id: '123' };
+const exampleUser = { id: '123' }
 
 describe('Trainer settings page', () => {
-  jest.mock('axios');
+  jest.mock('axios')
 
   beforeEach(async () => {
     axios.get.mockReturnValue(
       Promise.resolve({
         data: {
-          foundClient: { id: '111', name: 'john doe' },
           foundSession: {
             id: '222',
-            startTime: new Date(),
             endTime: new Date(),
+            startTime: new Date(),
           },
+          foundClient: { id: '111', name: 'john doe' },
         },
       })
-    );
+    )
 
     axios.delete = jest.fn((url, req) => {
-      if (
-        req.headers.id === exampleUser.id &&
-        url === '/api/trainer/cancel-session/'
-      ) {
-        return Promise.resolve({
-          data: { success: true },
-        });
+      if (req.headers.id === exampleUser.id && url === '/api/trainer/cancel-session/') {
+        return Promise.resolve({ data: { success: true } })
       }
-    });
+    })
 
     render(
       <Store>
@@ -43,31 +37,32 @@ describe('Trainer settings page', () => {
           <ManageSession match={{ params: { id: exampleUser.id } }} />
         </Router>
       </Store>
-    );
-    await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
-  });
-  afterEach(cleanup);
+    )
+    await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1))
+  })
+  
+  afterEach(cleanup)
 
   it('Should have three links ', async () => {
-    const links = screen.getAllByRole('link');
-    expect(links.length).toEqual(3);
-  });
+    const links = screen.getAllByRole('link')
+    expect(links.length).toEqual(3)
+  })
 
   it('Should have one button that says "cancel session"', async () => {
-    const btn = screen.getByRole('button');
-    expect(btn.textContent).toEqual('cancel session');
-  });
+    const btn = screen.getByRole('button')
+    expect(btn.textContent).toEqual('cancel session')
+  })
 
   it('Should make a delete request when cancel and confirm buttons are clicked"', async () => {
-    const btn = screen.getByRole('button');
+    const btn = screen.getByRole('button')
 
-    jest.spyOn(axios, 'delete');
+    jest.spyOn(axios, 'delete')
 
-    userEvent.click(btn);
-    const secondSetOfBtns = screen.getAllByRole('button');
-    const confirmBtn = secondSetOfBtns[1];
-    userEvent.click(confirmBtn);
-    await waitFor(() => expect(axios.delete).toHaveBeenCalledTimes(1));
-    expect(axios.delete).toHaveBeenCalledTimes(1);
-  });
-});
+    userEvent.click(btn)
+    const secondSetOfBtns = screen.getAllByRole('button')
+    const confirmBtn = secondSetOfBtns[1]
+    userEvent.click(confirmBtn)
+    await waitFor(() => expect(axios.delete).toHaveBeenCalledTimes(1))
+    expect(axios.delete).toHaveBeenCalledTimes(1)
+  })
+})
