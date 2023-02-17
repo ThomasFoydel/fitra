4911feed767be99642d268db61e3d68d239f42b0
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-17 16:39:25 -0500

    organized landing page

--- client/src/Pages/LandingPage/LandingPage.test.js
+++ client/src/Pages/LandingPage/LandingPage.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { render, screen, cleanup } from '@testing-library/react';
-
-import Store from 'context/Store';
-import LandingPage from './LandingPage';
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { render, screen, cleanup } from '@testing-library/react'
+import LandingPage from './LandingPage'
+import Store from 'context/Store'
 
 describe('Client landing page', () => {
   beforeEach(() => {
@@ -13,26 +12,28 @@ describe('Client landing page', () => {
           <LandingPage />
         </Router>
       </Store>
-    );
-  });
-  afterEach(cleanup);
+    )
+  })
+
+  afterEach(cleanup)
+
   it('Should have two links', () => {
-    const links = screen.getAllByRole('link');
-    expect(links.length).toEqual(2);
-  });
+    const links = screen.getAllByRole('link')
+    expect(links.length).toEqual(2)
+  })
 
   it('Should have a link to the trainers search page that says "Get Started"', () => {
-    const startLink = screen.getByText('Get Started');
-    expect(startLink.href).toEqual('http://localhost/trainers');
-  });
+    const startLink = screen.getByText('Get Started')
+    expect(startLink.href).toEqual('http://localhost/trainers')
+  })
 
   it('Should have a link to the trainer landing page that says "Coach Portal"', () => {
-    const coachPortalLink = screen.getByText('Coach Portal');
-    expect(coachPortalLink.href).toEqual('http://localhost/coachportal');
-  });
+    const coachPortalLink = screen.getByText('Coach Portal')
+    expect(coachPortalLink.href).toEqual('http://localhost/coachportal')
+  })
 
   it('Should have a header that says "Sharpen Your Body"', () => {
-    const heading = screen.getByRole('heading');
-    expect(heading.innerHTML).toEqual('SHARPEN<br>YOUR BODY');
-  });
-});
+    const heading = screen.getByRole('heading')
+    expect(heading.innerHTML).toEqual('SHARPEN<br>YOUR BODY')
+  })
+})
--- client/src/Pages/Messages/Messages.test.js
+++ client/src/Pages/Messages/Messages.test.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import { render, screen, cleanup } from '@testing-library/react';
-
-import Store from 'context/Store';
-import Messages from './Messages';
+import React from 'react'
+import { render, screen, cleanup } from '@testing-library/react'
+import Store from 'context/Store'
+import Messages from './Messages'
 
 describe('Client landing page', () => {
   beforeEach(() => {
@@ -10,14 +9,16 @@ describe('Client landing page', () => {
       <Store>
         <Messages />
       </Store>
-    );
-  });
-  afterEach(cleanup);
+    )
+  })
+
+  afterEach(cleanup)
+
   it('Should have a button that says "send"', () => {
-    screen.getByText('send');
-  });
+    screen.getByText('send')
+  })
 
   it('Should display "no messages" be default', () => {
-    screen.getByText('no messages');
-  });
-});
+    screen.getByText('no messages')
+  })
+})
