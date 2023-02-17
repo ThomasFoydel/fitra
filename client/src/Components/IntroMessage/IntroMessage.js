6f71347adb7a1ad1546bc681db904db45d547c67
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-17 18:37:37 -0500

    organized intromessage component

--- client/src/Components/IntroMessage/IntroMessage.js
+++ client/src/Components/IntroMessage/IntroMessage.js
@@ -1,70 +1,54 @@
-import React, { useState, useEffect, useContext } from 'react';
-import axios from 'axios';
-import { CTX } from 'context/Store';
-import './IntroMessage.scss';
+import axios from 'axios'
+import React, { useState, useEffect, useContext } from 'react'
+import { CTX } from 'context/Store'
+import './IntroMessage.scss'
 
 const IntroMessage = ({ id, toggle }) => {
-  const [appState, updateState] = useContext(CTX);
-  const [message, setMessage] = useState('');
-  const [complete, setComplete] = useState(false);
+  const [{ user }, updateState] = useContext(CTX)
+  const [complete, setComplete] = useState(false)
+  const [message, setMessage] = useState('')
 
-  const handleChange = ({ target: { value } }) => setMessage(value);
+  const handleChange = ({ target: { value } }) => setMessage(value)
 
   const handleSubmit = () => {
-    let valid = id && message && appState.user.id && appState.user.name;
-    if (!valid) return;
+    const valid = id && message && user.id && user.name
+    if (!valid) return
+
     const newMsg = {
-      userId: id,
       message,
-      sender: appState.user.id,
-      name: appState.user.name,
-      fromTrainer: appState.user.type === 'trainer',
-    };
+      userId: id,
+      name: user.name,
+      sender: user.id,
+      fromTrainer: user.type === 'trainer',
+    }
 
     axios.post('/api/message', newMsg).then(({ data }) => {
-      updateState({ type: 'NEW_MESSAGE', payload: { message: data } });
-      setComplete(true);
-    });
-  };
+      updateState({ type: 'NEW_MESSAGE', payload: { message: data } })
+      setComplete(true)
+    })
+  }
 
   useEffect(() => {
-    let subscribed = true;
-    if (complete) {
-      setTimeout(() => {
-        if (subscribed) toggle();
-      }, 2000);
-    }
-    return () => (subscribed = false);
-  }, [complete, toggle]);
+    let subscribed = true
+    if (complete) setTimeout(() => subscribed && toggle(), 2000)
+    return () => (subscribed = false)
+  }, [complete, toggle])
 
-  /* 
-
-  userId, message, sender, name 
-  
-  const newMessage = new Message({
-    authorName: name,
-    sender,
-    receiver: userId,
-    content: message,
-    participants: [sender, userId],
-  }); 
-
-  */
   return (
-    <div className='intro-message'>
+    <div className="intro-message">
       <h2>intro message</h2>
       <textarea
-        onChange={handleChange}
+        rows="10"
+        cols="30"
+        id="message"
+        name="message"
         value={message}
-        name=''
-        id=''
-        cols='30'
-        rows='10'
-      ></textarea>
+        onChange={handleChange}
+      />
       {complete && <p>message sent!</p>}
       <button onClick={handleSubmit}>send</button>
     </div>
-  );
-};
+  )
+}
 
-export default IntroMessage;
+export default IntroMessage
