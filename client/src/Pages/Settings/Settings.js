0197053cef81f380bb1b7b4ff7384fa2002e4a3a
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-17 16:09:40 -0500

    organized settings

--- client/src/Pages/Settings/Settings.js
+++ client/src/Pages/Settings/Settings.js
@@ -1,65 +1,65 @@
-import React, { useContext } from 'react';
-import './Settings.scss';
-import axios from 'axios';
-import { CTX } from 'context/Store';
-import { Link } from 'react-router-dom';
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { CTX } from 'context/Store'
+import './Settings.scss'
 
 const Settings = () => {
-  const [appState, updateState] = useContext(CTX);
-  const { type, token } = appState.user;
-  const { darkmode } = appState.settings;
+  const [appState, updateState] = useContext(CTX)
+  const { type, token } = appState.user
+  const { darkmode } = appState.settings
 
   const handleDarkMode = ({ target: { checked } }) => {
     axios
       .post(
         `/api/user/settings/${type}/darkmode`,
-        { checked: checked },
+        { checked },
         { headers: { 'x-auth-token': token } }
       )
       .then(({ data: { darkmode } }) => {
         updateState({
           type: 'CHANGE_SETTINGS',
           payload: { type: 'darkmode', value: darkmode },
-        });
+        })
       })
-      .catch((err) => console.log('darkmode error: ', err));
-  };
+      .catch((err) => console.error('darkmode error: ', err))
+  }
+
   return (
     <>
-      <div className='background' />
-      <div className='overlay' />
+      <div className="background" />
+      <div className="overlay" />
       <div className={`settings dm-${darkmode}`}>
-        <h2 className='header center'>Settings</h2>
-
-        <div className='form'>
-          <div className='setting-item'>
+        <h2 className="header center">Settings</h2>
+        <div className="form">
+          <div className="setting-item">
             <span>darkmode</span>
-            <label className='switch' htmlFor='darkmode'>
+            <label className="switch" htmlFor="darkmode">
               <input
-                data-testid='darkmode-btn'
+                data-testid="darkmode-btn"
                 checked={darkmode}
-                type='checkbox'
+                type="checkbox"
                 onChange={handleDarkMode}
-                id='darkmode'
+                id="darkmode"
               />
-              <span className='slider round'></span>
+              <span className="slider round"></span>
             </label>
           </div>
-          <div className='setting-item'>
-            <Link to='/terms-of-use'>terms of use</Link>
+          <div className="setting-item">
+            <Link to="/terms-of-use">terms of use</Link>
           </div>
-          <div className='setting-item'>
-            <Link to='/privacy-policy'>privacy policy</Link>
+          <div className="setting-item">
+            <Link to="/privacy-policy">privacy policy</Link>
           </div>
-          <div className='setting-item'>
-            <Link className='delete-btn' to='/delete_my_account'>
+          <div className="setting-item">
+            <Link className="delete-btn" to="/delete_my_account">
               delete my account
             </Link>
           </div>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Settings;
+export default Settings
