c0c59840736b68a9f3d7c238f00b550983b9fac8
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-17 16:46:54 -0500

    organized home

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,49 +1,46 @@
-import React, { useContext, useEffect, useState } from 'react';
-import axios from 'axios';
-import { CTX } from 'context/Store';
-import './Home.scss';
-
-import { config, animated, useTransition } from 'react-spring';
-
-import Session from './Session';
-import { act } from '@testing-library/react';
+import axios from 'axios'
+import { act } from '@testing-library/react'
+import { config, animated, useTransition } from 'react-spring'
+import React, { useContext, useEffect, useState } from 'react'
+import { CTX } from 'context/Store'
+import Session from './Session'
+import './Home.scss'
 
 const Home = () => {
-  const [appState] = useContext(CTX);
-  const { type } = appState.user;
-  const [sessions, setFoundSessions] = useState([]);
-  let { token } = appState.user;
+  const [{ user }] = useContext(CTX)
+  const { type, token } = user
+  const [sessions, setFoundSessions] = useState([])
 
   useEffect(() => {
-    let subscribed = true;
+    let subscribed = true
     axios
       .get(`/api/${type}/dashboard`, { headers: { 'x-auth-token': token } })
       .then(({ data: { sessions }, err }) => {
-        if (err) return console.log({ err });
+        if (err) return console.log({ err })
         if (subscribed && sessions)
           process.env.NODE_ENV === 'production'
             ? setFoundSessions(sessions)
-            : act(() => setFoundSessions(sessions));
+            : act(() => setFoundSessions(sessions))
       })
-      .catch((err) => console.log('connection error: ', err));
-    return () => (subscribed = false);
-  }, [token, type]);
+      .catch((err) => console.error('connection error: ', err))
+    return () => (subscribed = false)
+  }, [token, type])
 
   const animation = useTransition(sessions, (item) => item && item._id, {
+    trail: 200,
+    config: config.wobbly,
     from: { opacity: '0', transform: 'translateY(-20px)' },
     enter: { opacity: '1', transform: 'translateY(0px)' },
     leave: { opacity: '0', transform: 'translateY(-20px)' },
-    trail: 200,
-    config: config.wobbly,
-  });
+  })
 
   return (
     <>
-      <div className='background' />
-      <div className='overlay' />
-      <div className='home'>
+      <div className="background" />
+      <div className="overlay" />
+      <div className="home">
         <h2>Sessions</h2>
-        <div className='sessions'>
+        <div className="sessions">
           {sessions.length > 0 ? (
             animation.map(({ item, props, key }) => (
               <animated.div style={props} key={key}>
@@ -56,7 +53,7 @@ const Home = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
