33d4a7b6159e9d289f1b4de5c2c70518e787815b
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-17 16:29:21 -0500

    organized messages

--- client/src/Pages/Messages/Messages.js
+++ client/src/Pages/Messages/Messages.js
@@ -1,61 +1,58 @@
-import React, { useContext, useEffect, useState, useRef } from 'react';
-import { useSpring, config, animated } from 'react-spring';
-import ThreadListItem from './parts/ThreadListItem';
-import Thread from './parts/Thread';
-import ChatBox from './parts/ChatBox';
-import { CTX } from 'context/Store';
-import './Messages.scss';
+import { useSpring, config, animated } from 'react-spring'
+import React, { useContext, useEffect, useState, useRef } from 'react'
+import ThreadListItem from './parts/ThreadListItem'
+import ChatBox from './parts/ChatBox'
+import Thread from './parts/Thread'
+import { CTX } from 'context/Store'
+import './Messages.scss'
 
 const Messages = () => {
-  const [appState, updateState] = useContext(CTX);
-  let { messages } = appState;
-  let { token, id, name, type } = appState.user;
-  const [currentThread, setCurrentThread] = useState(null);
+  const [{ messages, user }, updateState] = useContext(CTX)
+  const { token, id, name, type } = user
+  const [currentThread, setCurrentThread] = useState(null)
 
-  const newMessage = (message) => {
-    updateState({ type: 'NEW_MESSAGE', payload: { message } });
-  };
+  const newMessage = (message) => updateState({ type: 'NEW_MESSAGE', payload: { message } })
 
-  const scrollRef = useRef();
+  const scrollRef = useRef()
 
-  const refEl = () => <div ref={scrollRef} />;
+  const refEl = () => <div ref={scrollRef} />
 
   useEffect(() => {
-    if (scrollRef.current)
-      scrollRef.current.scrollIntoView({ behavior: 'smooth' });
-  }, [messages, currentThread]);
-  let messageKeys = Object.keys(messages);
+    if (scrollRef.current) scrollRef.current.scrollIntoView({ behavior: 'smooth' })
+  }, [messages, currentThread])
+
+  const messageKeys = Object.keys(messages)
 
   const animation = useSpring({
+    config: config.smooth,
     opacity: currentThread ? 1 : 0,
     display: currentThread ? 'inherit' : 'none',
-    config: config.smooth,
-  });
+  })
 
   return (
-    <div className='messages'>
-      <div className='background' />
-      <div className='overlay' />
-      <div className='thread-section'>
-        {messageKeys.length === 0 && <div className='no-msg'>no messages</div>}
-        <div className='thread-list'>
+    <div className="messages">
+      <div className="background" />
+      <div className="overlay" />
+      <div className="thread-section">
+        {messageKeys.length === 0 && <div className="no-msg">no messages</div>}
+        <div className="thread-list">
           {messageKeys.map((key) => (
             <ThreadListItem
-              user={key}
               key={key}
-              setCurrentThread={setCurrentThread}
-              currentThread={currentThread}
+              user={key}
               token={token}
+              currentThread={currentThread}
+              setCurrentThread={setCurrentThread}
             />
           ))}
         </div>
-        <div className='current-thread'>
+        <div className="current-thread">
           {currentThread && (
             <Thread
               props={{
-                currentUser: appState.user.id,
-                thread: messages[currentThread],
                 refEl,
+                currentUser: id,
+                thread: messages[currentThread],
                 close: () => setCurrentThread(null),
               }}
             />
@@ -63,19 +60,19 @@ const Messages = () => {
         </div>
       </div>
 
-      <animated.div style={animation} className='chat-box'>
+      <animated.div style={animation} className="chat-box">
         <ChatBox
           props={{
-            isTrainer: type === 'trainer',
             userId: id,
-            userName: name,
             currentThread,
+            userName: name,
             update: newMessage,
+            isTrainer: type === 'trainer',
           }}
         />
       </animated.div>
     </div>
-  );
-};
+  )
+}
 
-export default Messages;
+export default Messages
