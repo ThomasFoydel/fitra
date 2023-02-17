5b790de4fdb60022c7cc294e2852ad4859b2e5e7
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-17 05:20:51 -0500

    refactored client profile

--- client/src/Pages/ClientProfile/ClientProfile.js
+++ client/src/Pages/ClientProfile/ClientProfile.js
@@ -1,82 +1,78 @@
-import React, { useState, useEffect, useContext, useRef } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { CTX } from 'context/Store';
-import Image from 'Components/Image/Image';
-import './ClientProfile.scss';
+import axios from 'axios'
+import { Link, useParams } from 'react-router-dom'
+import React, { useState, useEffect, useContext, useRef } from 'react'
+import Image from 'Components/Image/Image'
+import loadingSpin from 'imgs/loading/spin.gif'
+import { CTX } from 'context/Store'
+import './ClientProfile.scss'
 
-import loadingSpin from 'imgs/loading/spin.gif';
-
-const ClientProfile = ({
-  match: {
-    params: { id },
-  },
-}) => {
-  const [appState] = useContext(CTX);
-  let { token } = appState.user;
-  let belongsToCurrentUser = appState.user.id === id;
-
-  const [user, setUser] = useState(null);
-  const [err, setErr] = useState('');
+const ClientProfile = () => {
+  const { id } = useParams()
+  const [{ user }] = useContext(CTX)
+  const { token } = user
+  const belongsToCurrentUser = user.id === id
+  const [userData, setUserData] = useState(null)
+  const [err, setErr] = useState('')
 
   useEffect(() => {
-    let subscribed = true;
+    let subscribed = true
     axios
       .get(`/api/client/profile/${id}`, { headers: { 'x-auth-token': token } })
       .then(({ data: { foundUser, err } }) => {
-        if (err && subscribed) setErr(err);
-        else if (foundUser && subscribed) setUser(foundUser);
-      });
-    return () => (subscribed = false);
-  }, [id, token]);
+        if (err && subscribed) setErr(err)
+        else if (foundUser && subscribed) setUserData(foundUser)
+      })
+    return () => (subscribed = false)
+  }, [id, token])
 
-  const didMountRef = useRef(false);
+  const didMountRef = useRef(false)
   useEffect(() => {
-    let subscribed = true;
+    let subscribed = true
     if (didMountRef.current) {
       setTimeout(() => {
-        if (subscribed) setErr('');
-      }, 2700);
-    } else didMountRef.current = true;
-    return () => (subscribed = false);
-  }, [err]);
+        if (subscribed) setErr('')
+      }, 2700)
+    } else didMountRef.current = true
+    return () => (subscribed = false)
+  }, [err])
 
-  let { coverPic, profilePic, bio, name, displayEmail } = user || {};
+  const { coverPic, profilePic, bio, name, displayEmail } = userData || {}
+
   return (
-    <div className='clientprofile'>
-      {user && (
+    <div className="clientprofile">
+      {userData && (
         <>
           <div
-            className='cover-pic'
+            className="cover-pic"
             style={{
               backgroundImage: coverPic
                 ? ` linear-gradient(to bottom, rgba(0,0,0,0.2), rgba(0,0,0,0.9)), url("/api/image/${coverPic}")`
                 : '',
             }}
           >
-            <div className='info'>
+            <div className="info">
               <Image
                 src={profilePic ? `/api/image/${profilePic}` : loadingSpin}
                 alt={profilePic ? "user's profile" : 'loading profile'}
-                name='profile-pic'
+                name="profile-pic"
               />
-              <div className='section-1'>
-                <div className='name'>{name}</div>
-                <div className='email'>{displayEmail}</div>
-                {bio && <div className='bio'>{bio}</div>}
+              <div className="section-1">
+                <div className="name">{name}</div>
+                <div className="email">{displayEmail}</div>
+                {bio && <div className="bio">{bio}</div>}
                 {belongsToCurrentUser && (
-                  <Link to={`/editprofile`} className='link'>
+                  <Link to={`/editprofile`} className="link">
                     Edit Profile
                   </Link>
                 )}
               </div>
             </div>
           </div>
-          <div className='err'>{err}</div>
+          <div className="err">{err}</div>
         </>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default ClientProfile;
+export default ClientProfile
