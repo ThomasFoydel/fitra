3bce4ef46f19152904563bfa27bfa8c7d213243f
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-17 16:54:29 -0500

    organized edit profile page

--- client/src/Pages/EditProfile/EditProfile.js
+++ client/src/Pages/EditProfile/EditProfile.js
@@ -1,116 +1,96 @@
-import React, { useState, useContext } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import './EditProfile.scss';
-import { CTX } from 'context/Store';
-import ImageUploader from 'Components/ImageUploader/ImageUploader';
-import Image from 'Components/Image/Image';
-import { act } from '@testing-library/react';
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+import { act } from '@testing-library/react'
+import React, { useState, useContext } from 'react'
+import ImageUploader from 'Components/ImageUploader/ImageUploader'
+import Image from 'Components/Image/Image'
+import { CTX } from 'context/Store'
+import './EditProfile.scss'
+
+const initFormData = { name: '', bio: '', displayEmail: '' }
 
 const EditProfile = () => {
-  const [appState, updateState] = useContext(CTX);
-  const { type } = appState.user;
-  let {
-    token,
-    coverPic,
-    profilePic,
-    name,
-    bio,
-    displayEmail,
-    id,
-  } = appState.user;
-  const [formInfo, setFormInfo] = useState({
-    name: name || '',
-    bio: bio || '',
-    displayEmail: displayEmail || '',
-  });
+  const [{ user }, updateState] = useContext(CTX)
+  const { token, coverPic, profilePic, name, bio, displayEmail, id, type } = user
 
-  const handleChange = (e) => {
-    let { id, value } = e.target;
-    setFormInfo({ ...formInfo, [id]: value });
-  };
+  const [formInfo, setFormInfo] = useState({
+    bio: bio || '',
+    name: name || '',
+    displayEmail: displayEmail || '',
+  })
+
+  const handleChange = ({ target: { id, value } }) => setFormInfo({ ...formInfo, [id]: value })
 
   const handleSubmit = () => {
     axios
-      .put(`/api/${type}/editprofile`, formInfo, {
-        headers: { 'x-auth-token': token },
-      })
+      .put(`/api/${type}/editprofile`, formInfo, { headers: { 'x-auth-token': token } })
       .then(({ data: { bio, coverPic, displayEmail, name, profilePic } }) => {
-        let filteredRes = { bio, coverPic, displayEmail, name, profilePic };
+        const filteredRes = { bio, coverPic, displayEmail, name, profilePic }
         if (process.env.NODE_ENV === 'production') {
-          updateState({
-            type: 'EDIT_PROFILE',
-            payload: { res: filteredRes },
-          });
-          setFormInfo({ name: '', bio: '', displayEmail: '' });
+          updateState({ type: 'EDIT_PROFILE', payload: { res: filteredRes } })
+          setFormInfo(initFormData)
         } else {
           act(() => {
-            updateState({
-              type: 'EDIT_PROFILE',
-              payload: { res: filteredRes },
-            });
-            setFormInfo({ name: '', bio: '', displayEmail: '' });
-          });
+            updateState({ type: 'EDIT_PROFILE', payload: { res: filteredRes } })
+            setFormInfo(initFormData)
+          })
         }
       })
-      .catch((err) => console.log('err: ', err));
-  };
+      .catch((err) => console.error('err: ', err))
+  }
   return (
     <>
-      <div className='background' />
-      <div className='edit-profile'>
+      <div className="background" />
+      <div className="edit-profile">
         <h2>Edit your profile</h2>
-        <Link
-          className='link'
-          to={`${type === 'trainer' ? '/trainer' : '/user'}/${id}`}
-        >
+        <Link className="link" to={`${type === 'trainer' ? '/trainer' : '/user'}/${id}`}>
           back to my profile
         </Link>
         <input
-          type='text'
-          id='name'
+          id="name"
+          type="text"
+          value={formInfo.name}
           onChange={handleChange}
           placeholder={name || 'name...'}
-          value={formInfo.name}
         />
         <input
-          type='text'
-          id='bio'
+          id="bio"
+          type="text"
+          value={formInfo.bio}
           onChange={handleChange}
           placeholder={bio || 'bio...'}
-          value={formInfo.bio}
         />
         <input
-          type='displayEmail'
-          id='displayEmail'
+          id="displayEmail"
+          type="displayEmail"
           onChange={handleChange}
-          placeholder={displayEmail || 'display email...'}
           value={formInfo.displayEmail}
+          placeholder={displayEmail || 'display email...'}
         />
-        <button className='submit-btn' onClick={handleSubmit}>
+        <button className="submit-btn" onClick={handleSubmit}>
           submit
         </button>
-        <div className='image-uploaders'>
-          <div className='uploader'>
+        <div className="image-uploaders">
+          <div className="uploader">
             <Image
-              name='edit-pic'
+              name="edit-pic"
               src={`/api/image/${profilePic || '5f4d8786efa2ae10b38bfe9e'}`}
-              alt='your profile'
+              alt="your profile"
             />
             <ImageUploader props={{ kind: 'profilePic' }} />
           </div>
-          <div className='uploader'>
+          <div className="uploader">
             <Image
-              name='edit-pic'
+              name="edit-pic"
+              alt="your cover"
               src={`/api/image/${coverPic || '5f4d8786efa2ae10b38bfe9e'}`}
-              alt='your cover'
             />
             <ImageUploader props={{ kind: 'coverPic' }} />
           </div>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default EditProfile;
+export default EditProfile
