a3ee83481acd42bb712170adb9f2c848ce3deaf7
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-17 19:45:11 -0500

    organized connect routes

--- routes/Connect.js
+++ routes/Connect.js
@@ -1,55 +1,49 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middlewares/auth');
+const express = require('express')
+const Session = require('../models/Session')
+const Trainer = require('../models/Trainer')
+const auth = require('../middlewares/auth')
+const Client = require('../models/Client')
 
-const Client = require('../models/Client');
-const Session = require('../models/Session');
-const Trainer = require('../models/Trainer');
+const router = express.Router()
 
-router.get('/:connectionId', auth, async (req, res) => {
-  const { connectionId } = req.params;
-  const { userId } = req.tokenUser;
-  const foundClient = await Client.findById(userId);
-  const foundTrainer = await Trainer.findById(userId);
+router.get('/:connectionId', auth, async ({ params, tokenUser }, res) => {
+  const { userId } = tokenUser
+  const { connectionId } = params
+  const foundClient = await Client.findById(userId)
+  const foundTrainer = await Trainer.findById(userId)
 
-  const noUserFound = !foundClient && !foundTrainer;
-  if (noUserFound) return res.send({ err: 'user info not found' });
+  const noUserFound = !foundClient && !foundTrainer
+  if (noUserFound) return res.send({ err: 'user info not found' })
 
-  let foundSession;
+  let foundSession
   try {
-    foundSession = await Session.findById(connectionId).select('+roomId');
+    foundSession = await Session.findById(connectionId).select('+roomId')
   } catch (err) {
-    return res.send({ err: 'no session found' });
+    return res.send({ err: 'Invalid session id' })
   }
 
-  if (!foundSession) return res.send({ err: 'no session found' });
-  const { roomId } = foundSession;
+  if (!foundSession) return res.send({ err: 'no session found' })
+  const { roomId } = foundSession
 
-  const isClient = foundSession.client === userId;
-  const isTrainer = foundSession.trainer === userId;
-  const userNotOnSession = !isClient && !isTrainer;
-  if (userNotOnSession)
-    return res.send({
-      err: 'you are not logged in as a member of this session',
-    });
+  const isClient = foundSession.client === userId
+  const isTrainer = foundSession.trainer === userId
+  const userNotOnSession = !isClient && !isTrainer
+  if (userNotOnSession) {
+    return res.send({ err: 'you are not logged in as a member of this session' })
+  }
 
-  let { startTime, endTime } = foundSession;
-  let startDate = new Date(startTime);
-  let endDate = new Date(endTime);
-  let currentTime = new Date(Date.now());
-  let started = currentTime > startDate;
-  let ended = currentTime > endDate;
-  let active = started && !ended;
+  const { startTime, endTime } = foundSession
+  const startDate = new Date(startTime)
+  const endDate = new Date(endTime)
+  const currentTime = new Date(Date.now())
+  const started = currentTime > startDate
+  const ended = currentTime > endDate
+  const active = started && !ended
 
-  if (!started) return res.send({ err: 'this session has not yet begun' });
-  if (!active) return res.send({ err: 'this session has ended' });
+  if (!started) return res.send({ err: 'this session has not yet begun' })
+  if (!active) return res.send({ err: 'this session has ended' })
 
-  // change status of session if !started or !active
-  // if !started and time until session is less than 5hrs
-  // then send back time until session starts
-  // and display a countdown on the front end
-  // cause page to reload when timer hits zero
-  return res.send({ isClient, isTrainer, roomId });
-});
+  return res.send({ isClient, isTrainer, roomId })
+})
 
-module.exports = router;
+module.exports = router
