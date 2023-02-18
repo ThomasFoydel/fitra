3a4730449f1849106ff0af955a4b5a6ffef653a9
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-17 20:10:53 -0500

    organized session routes

--- routes/Session.js
+++ routes/Session.js
@@ -1,81 +1,67 @@
-const express = require('express');
-const { v4: uuidV4 } = require('uuid');
+const express = require('express')
+const { v4: uuidV4 } = require('uuid')
+const Trainer = require('../models/Trainer')
+const Session = require('../models/Session')
+const auth = require('../middlewares/auth')
+const Client = require('../models/Client')
 
-const auth = require('../middlewares/auth');
-const Trainer = require('../models/Trainer');
-const Client = require('../models/Client');
-const Session = require('../models/Session');
-
-const router = express.Router();
+const router = express.Router()
 
 router.post(
   '/new',
   auth,
-  async (
-    { body: { trainer, startTime, endTime, order }, tokenUser: { userId } },
-    res
-  ) => {
-    if (userId === trainer)
-      return res.send({ err: 'you cannot book yourself!' });
+  async ({ body: { trainer, startTime, endTime, order }, tokenUser: { userId } }, res) => {
+    if (userId === trainer) return res.send({ err: 'you cannot book yourself!' })
 
-    const errTrainer = await Trainer.findById(userId);
-    if (errTrainer) return res.send({ err: 'only clients can book trainers' });
+    const errTrainer = await Trainer.findById(userId)
+    if (errTrainer) return res.send({ err: 'only clients can book trainers' })
 
-    const foundClient = await Client.findById(userId);
-    if (!foundClient) return res.send({ err: 'user info not found' });
-    const foundTrainer = await Trainer.findById(trainer);
+    const foundClient = await Client.findById(userId)
+    if (!foundClient) return res.send({ err: 'user info not found' })
+    const foundTrainer = await Trainer.findById(trainer)
 
-    let { availability } = foundTrainer;
-    let sessions = await Session.find({ trainer: foundTrainer._id });
+    const { availability } = foundTrainer
+    const sessions = await Session.find({ trainer: foundTrainer._id })
 
-    let timeBlocked = false;
-    let rStart = new Date(startTime);
-    let rEnd = new Date(endTime);
+    let timeBlocked = false
+    const rStart = new Date(startTime)
+    const rEnd = new Date(endTime)
+
     availability.forEach((a) => {
-      let startIsBetween =
-        rStart >= new Date(a.startDate) && rStart < new Date(a.endDate);
-      let endIsBetween =
-        rEnd >= new Date(a.startDate) && rEnd <= new Date(a.endDate);
-      let blockedTimeIsBetween =
-        new Date(a.startDate) >= rStart && new Date(a.endDate) <= rEnd;
-      if (startIsBetween || endIsBetween || blockedTimeIsBetween) {
-        timeBlocked = true;
-      }
-    });
-    sessions.forEach((t) => {
-      let startIsBetween = rStart >= t.startTime && rStart < t.endTime;
-      let endIsBetween = rEnd >= t.startTime && rEnd <= t.endTime;
-      let blockedTimeIsBetween = t.startTime >= rStart && t.endTime <= rEnd;
-      if (startIsBetween || endIsBetween || blockedTimeIsBetween) {
-        timeBlocked = true;
-      }
-    });
-    if (timeBlocked) return res.send({ err: 'selected time is unavailable' });
+      const startIsBetween = rStart >= new Date(a.startDate) && rStart < new Date(a.endDate)
+      const endIsBetween = rEnd >= new Date(a.startDate) && rEnd <= new Date(a.endDate)
+      const blockedTimeIsBetween = new Date(a.startDate) >= rStart && new Date(a.endDate) <= rEnd
+      if (startIsBetween || endIsBetween || blockedTimeIsBetween) timeBlocked = true
+    })
 
-    const roomId = uuidV4();
+    sessions.forEach((t) => {
+      const startIsBetween = rStart >= t.startTime && rStart < t.endTime
+      const endIsBetween = rEnd >= t.startTime && rEnd <= t.endTime
+      const blockedTimeIsBetween = t.startTime >= rStart && t.endTime <= rEnd
+      if (startIsBetween || endIsBetween || blockedTimeIsBetween) timeBlocked = true
+    })
+
+    if (timeBlocked) return res.send({ err: 'selected time is unavailable' })
+
+    const roomId = uuidV4()
+
     const newSession = new Session({
+      order,
+      roomId,
+      endTime,
+      startTime,
       status: 'scheduled',
       client: foundClient._id,
       trainer: foundTrainer._id,
-      startTime,
-      endTime,
-      roomId,
-      order,
-    });
+    })
 
     newSession
       .save()
-      .then((result) => {
-        let { startTime, endTime, status, trainer, client } = result;
-        res.send({
-          newSession: { startTime, endTime, status, trainer, client },
-        });
+      .then(({ startTime, endTime, status, trainer, client }) => {
+        res.send({ newSession: { startTime, endTime, status, trainer, client } })
       })
-      .catch((err) => {
-        console.log('new session creation error: ', err);
-        return res.send({ err: 'database error' });
-      });
+      .catch(() => res.send({ err: 'database error' }))
   }
-);
+)
 
-module.exports = router;
+module.exports = router
