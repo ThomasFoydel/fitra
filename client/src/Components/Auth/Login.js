c7347ae85881c82d4b74b7a57d7c2c1b33a6e23a
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-19 04:03:37 -0500

    wrapped login in actual form

--- client/src/Components/Auth/Login.js
+++ client/src/Components/Auth/Login.js
@@ -17,13 +17,12 @@ const Login = ({ props: { setCurrentShow, setAuthOpen, trainer } }) => {
   const type = trainer ? 'trainer' : 'client'
 
   const handleSubmit = (e) => {
+    e.preventDefault()
     axios
       .post(`/api/${type}/login`, userForm)
       .then(({ data: { user, token } }) => updateState({ type: 'LOGIN', payload: { user, token } }))
       .catch(({ response: { data } }) => toast.error(data.message))
   }
-
-  const handleKeyDown = (e) => e.charCode === 13 && handleSubmit()
 
   const fbResponse = ({ accessToken, userID }) => {
     if (!accessToken || !userID) return toast.error('One or more fields missing')
@@ -44,55 +43,41 @@ const Login = ({ props: { setCurrentShow, setAuthOpen, trainer } }) => {
   const googleHandleError = () => setGoogleErr(true)
 
   return (
-    <>
-      <div className="login">
-        <button className="closeauth-btn" onClick={() => setAuthOpen(false)}>
-          <i className="fas fa-times fa-3x close-btn"></i>
-        </button>
-        <h2>{trainer && 'Trainer '}Login</h2>
-        <input
-          id="email"
-          type="text"
-          placeholder="email"
-          onChange={handleChange}
-          onKeyPress={handleKeyDown}
-        />
-        <input
-          id="password"
-          type="password"
-          placeholder="password"
-          onChange={handleChange}
-          onKeyPress={handleKeyDown}
-        />
-        <p className="tos">
-          by logging in, you agree to our{' '}
-          <a target="_blank" href="/terms-of-use">
-            Terms Of Use
-          </a>
-        </p>
-        <p className="pp">
-          and our{' '}
-          <a target="_blank" href="/privacy-policy">
-            Privacy Policy
-          </a>
-        </p>
-        <button className="submit-btn" onClick={handleSubmit}>
-          Login
-        </button>
+    <form className="login" onSubmit={handleSubmit}>
+      <button className="closeauth-btn" onClick={() => setAuthOpen(false)}>
+        <i className="fas fa-times fa-3x close-btn"></i>
+      </button>
+      <h2>{trainer && 'Trainer '}Login</h2>
+      <input id="email" type="text" placeholder="email" onChange={handleChange} />
+      <input id="password" type="password" placeholder="password" onChange={handleChange} />
+      <p className="tos">
+        by logging in, you agree to our{' '}
+        <a target="_blank" href="/terms-of-use">
+          Terms Of Use
+        </a>
+      </p>
+      <p className="pp">
+        and our{' '}
+        <a target="_blank" href="/privacy-policy">
+          Privacy Policy
+        </a>
+      </p>
+      <button className="submit-btn" type="submit">
+        Login
+      </button>
 
-        {!trainer && (
-          <>
-            <Facebook props={{ callback: fbResponse }} />
-            <Google props={{ googleHandleSuccess, googleHandleError, googleErr }} />
-          </>
-        )}
+      {!trainer && (
+        <>
+          <Facebook props={{ callback: fbResponse }} />
+          <Google props={{ googleHandleSuccess, googleHandleError, googleErr }} />
+        </>
+      )}
 
-        <button onClick={() => setCurrentShow('register')}>
-          No account? <strong>Sign up</strong>
-        </button>
-        <AuthPageToggle />
-      </div>
-    </>
+      <button onClick={() => setCurrentShow('register')}>
+        No account? <strong>Sign up</strong>
+      </button>
+      <AuthPageToggle />
+    </form>
   )
 }
 
